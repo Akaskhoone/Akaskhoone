9b16cff1d7eb4e2c0178b029ef62b3f985283495
Abolfazl Amiri <aa.smpro@gmail.com>
2018-08-19 09:37:01 +0430

    DRF added, users serializers created

--- akaskhoone/settings.py
+++ akaskhoone/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'users.apps.UsersConfig',
 ]
 
---
+++ users/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import User, Profile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
