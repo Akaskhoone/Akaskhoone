993b6d469536cf10096e52c59fdbed322374b2ea
Abolfazl Amiri <aa.smpro@gmail.com>
2018-08-20 03:34:23 +0430

    tests for user login api endpoint added [TTHREE-40]

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from rest_framework_jwt.settings import api_settings
 
-# Create your tests here.
+from users.serializers import *
+
+
+class APIJWTClient(APIClient):
+    def login(self, **credentials):
+        response = self.post(reverse("api:v0:login"), credentials, format='json')
+        if response.status_code == status.HTTP_200_OK:
+            self.credentials(
+                HTTP_AUTHORIZATION="{0} {1}".format(api_settings.JWT_AUTH_HEADER_PREFIX, response.data['token']))
+            return True, response
+        else:
+            return False, response
+
+
+class APIJWTTestCase(APITestCase):
+    client_class = APIJWTClient
+
+
+class APILoginTest(APIJWTTestCase):
+    def test_login_with_real_user(self):
+        print('>>> test login with real user')
+        User.objects.create_user('aasmpro', 'aasmpro@admin.com', 'passaasmpro')
+        response = self.client.post(reverse("api:v0:login"), {'email': 'aasmpro@admin.com', 'password': 'passaasmpro'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'token')
+
+    def test_login_with_fake_user(self):
+        print('>>> test login with fake user')
+        response = self.client.post(reverse("api:v0:login"), {'email': 'aasmpro@admin.com', 'password': 'passaasmpro'})
+        self.assertEqual(response.status_code, 400)
+
+
+class APIAuthorizationTest(APIJWTTestCase):
+    def test_api_authorization_needed_endpoint(self):
+        print('>>> test api authorization needed endpoint')
+        response = self.client.get(reverse("api:v0:users"))
+        self.assertEqual(response.status_code, 401)
+        User.objects.create_user('aasmpro', 'aasmpro@admin.com', 'passaasmpro')
+        self.client.login(email='aasmpro@admin.com', password='passaasmpro')
+        # response = self.client.post(reverse("api:v0:login"), {'email': 'aasmpro@admin.com', 'password': 'passaasmpro'})
+        # authorization = F"{api_settings.JWT_AUTH_HEADER_PREFIX} {response.data['token']}"
+        # self.client.credentials(HTTP_AUTHORIZATION=authorization)
+        response = self.client.get(reverse("api:v0:users"))
+        self.assertEqual(response.status_code, 200)
--- api/urls.py
+++ api/urls.py
@@ -2,13 +2,13 @@ from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 from api import views
 
-app_name = "api"
+app_name = 'api'
 
 urlpatterns = [
     path('api/v0/', include(([
-        path('users/', views.Users.as_view()),
-        path('login/', obtain_jwt_token),
-        path('refresh/', refresh_jwt_token),
-        path('verify/', verify_jwt_token),
-    ], 'api'), namespace='api')),
+        path('users/', views.Users.as_view(), name='users'),
+        path('login/', obtain_jwt_token, name='login'),
+        path('refresh/', refresh_jwt_token, name='refresh'),
+        path('verify/', verify_jwt_token, name='verify'),
+    ], 'v0'))),
 ]
