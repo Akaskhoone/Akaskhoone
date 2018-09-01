c402c4797b29ec37bdc805316778f85279720280
Abolfazl Amiri <aa.smpro@gmail.com>
2018-09-01 16:59:08 +0430

    utils APIJWTClient fixed

--- accounts/api/utils.py
+++ accounts/api/utils.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.settings import api_settings
 
 
 class APIJWTClient(APIClient):
-    def login(self, url="/api/token/", get_response=False, token="access", auth_header_type=0, **credentials):
+    def login(self, url="/api/v0/accounts/login/", get_response=True, token="access", auth_header_type=0, **credentials):
         auth_header_type = auth_header_type if auth_header_type < len(api_settings.AUTH_HEADER_TYPES) else 0
-        response = self.post(url, credentials, format='json')
+        response = self.post(path=url, data=credentials, format='json')
         if response.status_code == status.HTTP_200_OK:
             self.credentials(
                 HTTP_AUTHORIZATION="{0} {1}".format(
