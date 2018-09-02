57724f608da9586e81bfd3ea21c5d810e02f684a
Abolfazl Amiri <aa.smpro@gmail.com>
2018-09-02 09:41:22 +0430

    Serving static files in debug mode

--- akaskhoone/urls.py
+++ akaskhoone/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,6 @@ urlpatterns = [
     # path('', include('posts.urls')),
     # path('', include('api.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
